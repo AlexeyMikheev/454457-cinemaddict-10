084e5b784b1f618f5916742d6d539a01486eaff2
Alexey Miheev <almiheev88@gmail.com>
2019-12-19 10:44:51 +0300

    Prepare add statistic filters

--- src/components/statistic.js
+++ src/components/statistic.js
@@ -19,19 +19,19 @@ const getStatisticTemplate = (totalFilms, totalDuration, topGenreName) => {
   <form action="https://echo.htmlacademy.ru/" method="get" class="statistic__filters">
     <p class="statistic__filters-description">Show stats:</p>
 
-    <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-all-time" value="all-time" checked>
+    <input type="radio" date-period="${Period.ALL}" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-all-time" value="all-time" checked>
     <label for="statistic-all-time" class="statistic__filters-label">All time</label>
 
-    <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-today" value="today">
+    <input type="radio" date-period="${Period.TODAY}" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-today" value="today">
     <label for="statistic-today" class="statistic__filters-label">Today</label>
 
-    <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-week" value="week">
+    <input type="radio" date-period="${Period.WEEK}" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-week" value="week">
     <label for="statistic-week" class="statistic__filters-label">Week</label>
 
     <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-month" value="month">
-    <label for="statistic-month" class="statistic__filters-label">Month</label>
+    <label for="statistic-month" date-period="${Period.MONTH}" class="statistic__filters-label">Month</label>
 
-    <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-year" value="year">
+    <input type="radio" date-period="${Period.YEAR}" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-year" value="year">
     <label for="statistic-year" class="statistic__filters-label">Year</label>
   </form>
 
